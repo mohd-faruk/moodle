02d50ebe4aee3cf9ab6f9c985d3843d99a5e6ad2
Mohd Faruk <mohdfaruk70170@gmail.com>
2026-09-21 03:56:20 +0530

    MDL-89846 admin: Restore favicon on admin pages with conditional setting

--- admin/category.php
+++ admin/category.php
@@ -120,7 +120,7 @@ foreach ($settingspage->children as $childpage) {
                 $outputhtml .= $setting->output_html($data);
                 if ($childpage->has_dependencies()) {
                     $context = ['dependencies' => json_encode($childpage->get_dependencies_for_javascript())];
-                    echo $OUTPUT->render_from_template('core_admin/settings_showhide', $context);
+                    $outputhtml .= $OUTPUT->render_from_template('core_admin/settings_showhide', $context);
                 }
             }
             $outputhtml .= html_writer::end_tag('fieldset');
